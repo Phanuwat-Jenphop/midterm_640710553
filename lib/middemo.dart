444f9195ddc83db549b0249af31dca26a817d2fc
Phanu_Jen <jenphop_p@silpakorn.edu>
2024-01-27 00:45:55 +0700

    first commit

---
+++ lib/middemo.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var num = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+       
+      ),
+      body: Center(
+       
+        child: Column(      
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            
+          ],
+        ),
+      ), 
+    );
+  }
+}
